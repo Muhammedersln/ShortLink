735f307591344fc919045cd806e6aa9eb333b22d
Muhammed Eraslan <muhammed.ersln@icloud.com>
2024-07-20 19:13:07 +0300

    Add project files.

---
+++ ShortLink.Client/Controllers/UrlController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ShortLink.Client.Data.Models;
+using ShortLink.Client.Data.ViewModels;
+
+namespace ShortLink.Client.Controllers
+{
+    public class UrlController : Controller
+    {
+        public IActionResult Index()
+        {
+            //fake data
+            var allUrls = new List<GetUrlVM>()
+            {
+                new GetUrlVM { Id = 1, OriginalLink = "https://www.google.com", ShortLink = "sh1",NrOfClicks=1,UserId=1 },
+                new GetUrlVM { Id = 2, OriginalLink = "https://www.youtube.com", ShortLink = "sh2",NrOfClicks=5,UserId=2 },
+                new GetUrlVM { Id = 3, OriginalLink = "https://www.facebbook.com", ShortLink = "sh3",NrOfClicks=9,UserId=3},
+            };
+            return View(allUrls);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        public IActionResult Remove(int id)
+        {
+            return View();
+        }
+    }
+}
---
+++ ShortLink.Client/Data/Models/User.cs
@@ -0,0 +1,16 @@
+﻿namespace ShortLink.Client.Data.Models
+{
+    public class User
+    {
+
+        public User()
+        {
+            Urls = new List<Url>();
+        }
+
+        public int Id { get; set; }
+        public string Email { get; set; }
+
+        public List<Url> Urls { get; set; }
+    }
+}
